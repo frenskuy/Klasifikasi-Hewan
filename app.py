4d7073dc7664e84de9de0f8aaa9a1cecce5ff86d
Frenky <116736412+frenskuy@users.noreply.github.com>
2025-07-26 12:47:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ uploaded_file = st.file_uploader("Upload a cat or dog image", type=["jpg", "jpeg
 # Prediction logic
 if uploaded_file is not None:
     try:
-        image = Image.open(file).convert("RGB")
+        image = Image.open(uploaded_file).convert("RGB")
         st.image(image, caption="Uploaded Image", use_container_width=True)
 
         st.markdown("### Prediction")
